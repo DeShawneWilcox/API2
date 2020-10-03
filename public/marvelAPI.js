5ad237bc3b2383ab21808648b63847f1883ef1fd
DeShawne <dmw606@gmail.com>
2020-10-02 20:47:10 -0400

    2nd commit

--- public/marvelAPI.js
+++ public/marvelAPI.js
@@ -1,4 +1,4 @@
-const baseURL = "http://gateway.marvel.com"
+const baseURL = "http://gateway.marvel.com/"
 const apikey = "66eedb387295bf29b8cb125ed88436c1"
 
 let url;
@@ -7,83 +7,80 @@ let marvelSearch = document.querySelector('.search');
 let searchForm = document.querySelector('form');
 let submitBtn = document.querySelector('.submit');
 let section = document.querySelector('section');
+let hash = 'c01549da7b564f9b8f982987362c2da1';
 
-searchForm.addEventListener('submit' , displayResults); 
+searchForm.addEventListener('submit' , fetchResults); 
 
-// let characters = [];
+let characters = [];
 
-// async function getCharacters() {
-//     for(let offset = 0; offset <= 1400; offset += 100) {
-//         let res =await fetch(`${baseUrl}v1/public/characters?apikey=${apikey}&limit=100&offset=${offset}`);
-//         let json = await res.json()
-//         console.log(json);
-//         characters.push(...json.data.results);
-//     }
-//     console.log(characters);
-// }
-
-function fetchResults(e) {
-    console.log(e);
+async function fetchResults(e) {
     e.preventDefault();
-
-    url = baseURL + '?api-key' + apikey + '&q=' + marvelSearch.value;
-
-    fetch(url)
-    .then(function(result) {
-        console.log(result);
-        return result.json();
-    })
-    .then(function(json) {
-        console.log(json);
-        displayResults(json);
-    })
+    for(let offset = 0; offset <= 1400; offset += 100) {
+        let res =await fetch(`${baseURL}v1/public/characters?ts=1&apikey=${apikey}&hash=${hash}&limit=100&offset=${offset}`);
+        let json = await res.json()
+        // console.log(json);
+        characters.push(...json.data.results);
+    }
+    displayResults(characters);
+    // console.log(characters);
 }
 
+
+
 function displayResults(json) {
-    console.log('displayResults', json);
+    // console.log('displayResults', json);
     while(section.firstChild) {
         section.removeChild(section.firstChild);
     }
 
-    let profile = json;
-
+    let profile = characters;
+    characters.forEach(c => {
+        
+        if (c.name.toLowerCase().includes(marvelSearch.value.toLowerCase()) ) {
+            console.log(c.name);
+            console.log(c.thumbnail.path+'.'+c.thumbnail.extension);
+            console.log(c.description);
+            console.log(c.series);
+            
+            const picture = document.createElement('img');
+            const characterName = document.createElement('h1');
+            // const series = document.createElement('h3');
+            const description = document.createElement('p');
+        
+        
+            picture.src = c.thumbnail.path+"/portrait_xlarge."+c.thumbnail.extension;
+            picture.alt = `${c.name}`; 
+            // picture.style = ;
+            
+            characterName.className = 'CharacterName';
+            characterName.innerText = c.name;
+            // // // name.style = ;
+        
+            // //id.className = 'characterId';
+            // // id.innerText = `${profile.Character.id}`;
+            // // // id.style = ;
+        
+            // series.className = 'marvelSeries';
+            // series.innerText = c.series;
+            // // // series.style = ;
+        
+            description.className = 'characterDescription';
+            description.innerText = c.description;
+            // // // description.style = ;
+        
+            section.appendChild(picture);
+            section.appendChild(characterName);
+            // // section.appendChild(id);
+            // section.appendChild(series);
+            section.appendChild(description);
+        }
+    })
     if (profile.length === 0) {
         console.log('no results');
-    } else {
-        // const picture = document.createElement('img');
-        const characterName = document.createElement('h1');
-        const id = document.createElement('h3');
-        const series = document.createElement('h3');
-        const description = document.createElement('p');
-
-
-        // picture.src = profile.Character.thumbnail;
-        // picture.alt = profile.Character.name; 
-        // picture.style = ;
+    } 
         
-        characterName.className = 'CharacterName';
-        characterName.innerText = profile.Character.name;
-        // name.style = ;
 
-        id.className = 'characterId';
-        id.innerText = `${profile.Character.id}`;
-        // id.style = ;
-
-        series.className = 'marvelSeries';
-        series.innerText = `${profile.character.series}`;
-        // series.style = ;
-
-        description.className = 'characterDescription';
-        description.innerText = `${profile.character.description}`;
-        // description.style = ;
-
-        // section.appendChild(picture);
-        section.appendChild(characterName);
-        section.appendChild(id);
-        section.appendChild(series);
-        section.appendChild(description);
-
-        fetch(url);
-    }
+        // fetch(url);
+    
     
 }
